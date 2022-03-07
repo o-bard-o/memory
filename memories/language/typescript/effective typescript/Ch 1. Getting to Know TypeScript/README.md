955451db094e8befd0f68b462f7f6ac168adf01f
yongwookLee <yongw00k@g.skku.edu>
2022-03-07 20:10:39 +0900

    :memo: Add 'Effective typescript/Item 1'

---
+++ memories/language/typescript/effective typescript/Ch 1. Getting to Know TypeScript/README.md
@@ -0,0 +1,3 @@
+# Getting to Know TypeScript
+
+[Item 1: Understand the Relationship Between TypeScript and JavaScript]("./Item 1")
---
+++ memories/language/typescript/effective typescript/Item 1/03.ts
@@ -0,0 +1,5 @@
+const a = null + 7; // 자바스크립트에서는 a 값이 7이 됩니다.
+
+const b = [] + 12; // 자바스크립트에서는 b 값이 '12'가 됩니다.
+
+alert("Hello", "TypeScript"); // 자바스크립트에서는 "Hello" 경고를 표시합니다.
