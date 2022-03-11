0c6ef318d597caee827251b5a03b6901275008b9
yongwookLee <yongw00k@g.skku.edu>
2022-03-12 00:09:59 +0900

    :clown_face: Mock useState

--- memories/interview/shopping-mall-spa/src/core/react.js
+++ memories/interview/shopping-mall-spa/src/core/react.js
@@ -1,28 +1,48 @@
-export function render(vdom, container){
+const hooks = [];
+let currentComponent = -1;
+
+export const render = (vdom, container) => {
   container.appendChild(renderDOM(vdom));
 };
 
-export function renderDOM(vdom){
+export const useState = (initValue) => {
+  const position = currentComponent;
+
+  if(!hooks[position]) {
+    hooks[position] = initValue;
+  }
+  
+  return [
+    hooks[position],
+    (nextValue) => {
+      hooks[position] = nextValue;
+    }
+  ]
+}
+
+export const E = (tagName, props, ...children) => {
+  if(typeof tagName === 'function') {
+    currentComponent++;
+    return tagName.apply(null, [props, ...children]);
+  }
+  return { tagName, props, children };
+};
+
+const renderDOM = (vdom) => {
   if (typeof vdom === "string") {
     return document.createTextNode(vdom);
   }
 
   if (vdom === undefined) return;
 
-  if(typeof vdom.tagName === "string") {
-    const $el = document.createElement(vdom.tagName);
-    vdom.children.map(renderDOM).forEach((node) => {
-      $el.appendChild(node);
-    });
-    return $el;
-  } else {
-    const $el = renderDOM(vdom.tagName());
-    return $el;
+  const $el = document.createElement(vdom.tagName);
+  if(vdom && vdom.props !== null) {
+    vdom.props.forEach((i) => {
+      $el.setAttribute(i.k, i.v);
+    })
   }
+  vdom.children.map(renderDOM).forEach((node) => {
+    $el.appendChild(node);
+  });
+  return $el;    
 };
-
-const E = (tagName, props = null, children = []) => {
-  return { tagName, props, children };
-};
-
-export default E;
